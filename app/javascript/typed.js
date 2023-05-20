d731136ad6db450cdd2cb59e89302ae629885356
marco <enidev911@gmail.com>
2023-05-20 02:37:06 -0400

    retoques

---
+++ app/javascript/typed.js
@@ -0,0 +1,9 @@
+if (location.pathname == "/" || (window.location.href.indexOf("Index") > -1)){
+	var typed = new Typed('.typed', {
+		strings: ['<span class="text-dark">Portafolio</span>','<span class="text-primary">Marco Contreras</span>', '<i class="text-danger"><b>F</b>ullstack <b>R</b>uby <b>O</b>n <b>R</b>ails<i>'],
+		typeSpeed: 70,
+		loop: true,
+		backSpeed: 50
+	});
+}
+
